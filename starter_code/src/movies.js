6c2a6eeee7606dd3b38bb0ede1ad0a99c37f44f6
Jorge Quintana <jquintana21299@columbushs.com>
2019-11-19 16:08:49 -0500

    starting

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,10 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+movies.year.sort( (a, b) =>{
+    return a - b;
+})
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles

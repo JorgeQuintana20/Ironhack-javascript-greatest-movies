d1caab9a23859268c40371387e6e1b416d91a02c
Jorge Quintana <21299@columbushs.com>
2019-11-22 10:24:35 -0500

    third

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -42,28 +42,62 @@ function howManyMovies(anArray) {
 
 function orderAlphabetically(anArrayTwo) {
 
-    let alph = [...anArrayTwo];
-  
-    alph.sort(function (a, b){
-      if(a.title < b.title){
-        return -1;
-      }
-      else if(b.title < a.title){
-        return 1;
-      }
-      else{
-        return 0
-      }
-    })
-  
-    return alph
-  };
-  
-  console.log(orderAlphabetically(movies))
+  let alph = [...anArrayTwo];
+
+  let titles = alph.map( (eachTitle) =>{
+    return eachTitle.title;
+  })
+
+  titles.sort(function (a, b){
+    if(a < b){
+      return -1;
+    }
+    else if(b < a){
+      return 1;
+    }
+    else{
+      return 0
+    }
+  })
+
+  return titles.splice(0, 20);
+};
+
+console.log(orderAlphabetically(movies))
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(array){
+
+  if(array.length == 0){
+    return 0;
+  }
+
+  let total = array.reduce( (a, c) =>{
+    if(c.rate){
+      return a + c.rate;
+    }
+    else{
+      return a + 0;
+    }
+  }, 0);
+
+  let avgRate = (total / array.length).toFixed(2);
+  return Number(avgRate);
+
+}
+
+console.log(ratesAverage(movies))
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
+
+function dramaMovieRate(array) {
+
+  if(array.genre == "Drama") {
+    return 
+  }
+
+}
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 

b7d5f314b41202a565b510d87f3cb3dd6cda089e
Jorge Quintana <jquintana21299@columbushs.com>
2019-11-24 22:50:02 -0500

    final

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -91,14 +91,53 @@ console.log(ratesAverage(movies))
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
-function dramaMovieRate(array) {
-
-  if(array.genre == "Drama") {
-    return 
-  }
-
+function dramaMoviesRate(movieList) {
+  if (movieList.length === 0) {
+    return 0; }
+  let dramaMovies = movieList.filter(movie => movie.genre.includes('Drama'));
+  if (dramaMovies.length === 0) 
+  {return 0; }
+  let movieRates = dramaMovies.map(movie => movie.rate);
+  let checkedMovieRates = movieRates.filter(movie => movie !== undefined);
+  const totalReviews = checkedMovieRates.reduce((sum, elem) => {
+      return sum += elem;
+  }, 0);
+  return Math.round((totalReviews / checkedMovieRates.length) * 100) / 100;
 }
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+function turnHoursToMinutes(movieList) {
+  movieList.forEach(movie => {
+    let time = movie.duration.split(' ');
+    console.log(time);
+  });
+
+  let newList = movieList.map(movie => movie);
+
+  let changedDuration = newList.map((movie) => {
+      let time = movie.duration;
+      let min;
+      let hour;
+      let final;
+      let temp;
+      if (time.includes('h') && time.includes('min')) {
+          temp = time.replace('h', ' ').replace('min', ' ').trim();
+          let hour = Number(temp.substring(0, temp.indexOf(" ")));
+          let min = Number(temp.substring(temp.indexOf(" ") + 1));
+          final = (hour * 60 + min);
+      }
+      else if (time.includes('h')) {
+          hour = Number(time.substring(0, time.indexOf("h")));
+          final = hour * 60;
+      }
+      else if (time.includes('min')) {
+          min = Number(time.substring(0, time.indexOf("min")));
+          final = min;
+      }
+      return movieList
+  })
+  return changedDuration;
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
